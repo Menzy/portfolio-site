df66f9591be3eb4e1aa1df875b70196fc14daaab
Menzy <joshuamensah46@gmail.com>
2024-12-14 10:19:13 +0000

    Enhance Integrations section with reverse prop for dynamic display of integrations

--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -65,6 +65,7 @@ export default function Integrations() {
                             <IntegrationsColumn integrations={integrations} />
                             <IntegrationsColumn
                                 integrations={integrations.slice().reverse()}
+                                reverse
                                 className="hidden md:flex"
                             />
                         </div>
